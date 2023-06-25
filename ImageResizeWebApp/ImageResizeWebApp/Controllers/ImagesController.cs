4d0587ec3bb536f5b3139e4059942e690ec2504a
Matei Capan <matei@capan.ro>
2023-06-25 20:28:59 +0300

    test

--- ImageResizeWebApp/ImageResizeWebApp/Controllers/ImagesController.cs
+++ ImageResizeWebApp/ImageResizeWebApp/Controllers/ImagesController.cs
@@ -7,7 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
-using Microsoft.Azure.Data.Table;
+// using Microsoft.Azure.Data.Table;
 
 namespace ImageResizeWebApp.Controllers
 {
