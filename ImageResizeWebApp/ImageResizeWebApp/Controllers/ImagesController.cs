c79d942a8a52f7693d5fb632d39b2ec5fbfd7462
Matei Capan <matei@capan.ro>
2023-06-25 21:05:51 +0300

    test

--- ImageResizeWebApp/ImageResizeWebApp/Controllers/ImagesController.cs
+++ ImageResizeWebApp/ImageResizeWebApp/Controllers/ImagesController.cs
@@ -7,7 +7,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
-using Models;
 
 namespace ImageResizeWebApp.Controllers
 {
@@ -80,71 +79,19 @@ namespace ImageResizeWebApp.Controllers
             try
             {
                 if (storageConfig.AccountKey == string.Empty || storageConfig.AccountName == string.Empty)
-                    return BadRequest("Sorry, can't retrieve your Azure storage details from appsettings.js, make sure that you add Azure storage details there.");
+                    return BadRequest(
+                        "Sorry, can't retrieve your Azure storage details from appsettings.js, make sure that you add Azure storage details there.");
 
                 if (storageConfig.ImageContainer == string.Empty)
                     return BadRequest("Please provide a name for your image container in Azure blob storage.");
 
                 List<string> thumbnailUrls = await StorageHelper.GetThumbNailUrls(storageConfig);
-                return new ObjectResult(thumbnailUrls);            
+                return new ObjectResult(thumbnailUrls);
             }
             catch (Exception ex)
             {
                 return BadRequest(ex.Message);
             }
         }
-
-        // [HttpPost("/api/images/review")]
-        // public async Task<IActionResult> AddReview(string userReview, string userImageName)
-        // {
-            
-            // // Create a new ReviewEntity instance
-            // Guid uuid = Guid.NewGuid();
-            // string partitionKey = uuid.ToString();
-            // uuid = Guid.NewGuid()
-            // string rowKey = uuid.ToString();
-            // var reviewEntity = new ReviewEntity(partitionKey, rowKey)
-            // {
-            //     review = userReview,
-            //     imageName = userImageName
-            // };
-            // response = AzureTables.InsertEntity("blobstoragegb", "DefaultEndpointsProtocol=https;AccountName=blobstoragegb;AccountKey=RK9FSZy7Z1oyKtIbSy8qOilQXW22FwcofWwdp1DoMjchWZDm8R0FVd7BZfx2+xVGsan4/GADAMi6+AStoRfMoQ==;EndpointSuffix=core.windows.net", "tablestoragegb", JsonConvert.SerializeObject(reviewEntity));
-        // }
-        //     string storageConnectionString = "DefaultEndpointsProtocol=https;AccountName=blobstoragegb;AccountKey=RK9FSZy7Z1oyKtIbSy8qOilQXW22FwcofWwdp1DoMjchWZDm8R0FVd7BZfx2+xVGsan4/GADAMi6+AStoRfMoQ==;EndpointSuffix=core.windows.net";
-        //     CloudStorageAccount storageAccount = CloudStorageAccount.Parse(storageConnectionString);
-        //     CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
-        //     
-        //     string tableName = "tablestoragegb";
-        //     CloudTable table = tableClient.GetTableReference(tableName);
-        //     await table.CreateIfNotExistsAsync();
-        //     
-        //     Guid uuid = Guid.NewGuid();
-        //     string partitionKey = uuid.ToString();
-        //     uuid = Guid.NewGuid()
-        //     string rowKey = uuid.ToString();
-        //
-        //     var entity = new MyEntity(partitionKey, rowKey)
-        //     {
-        //         review = userReview,
-        //         imageName = userImageName
-        //     };
-        //     // // Create a reference to the table
-        //     // CloudTable table = tableClient.GetTableReference(tableName);
-        //     //
-        //     // // Create a new ReviewEntity instance
-        //     // var reviewEntity = new ReviewEntity("PartitionKey", "RowKey")
-        //     // {
-        //     //     Review = review,
-        //     //     ImageName = imageName
-        //     // };
-        //     //
-        //     // // Create an Insert operation
-        //     // TableOperation insertOperation = TableOperation.Insert(reviewEntity);
-        //     //
-        //     // // Execute the operation
-        //     // await table.ExecuteAsync(insertOperation);
-        //     //
-        //     // return Ok("Review added to table storage.");
-        // }
     }
 }
