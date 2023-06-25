b595739accfd2c4d7e579f03e021243a42a8f03a
Matei Capan <matei@capan.ro>
2023-06-25 21:26:43 +0300

    test

--- ImageResizeWebApp/ImageResizeWebApp/Controllers/ImagesController.cs
+++ ImageResizeWebApp/ImageResizeWebApp/Controllers/ImagesController.cs
@@ -105,6 +105,7 @@ namespace ImageResizeWebApp.Controllers
             //     Review = review,
             //     ImageName = imageName
             // };
+            return null;
         }
     }
 }
