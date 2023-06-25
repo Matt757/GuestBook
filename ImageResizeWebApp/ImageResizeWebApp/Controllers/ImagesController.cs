8a1f460d3e0bbf78b38e1bfaf599c28264d05e88
Matei Capan <matei@capan.ro>
2023-06-26 01:01:30 +0300

    test

--- ImageResizeWebApp/ImageResizeWebApp/Controllers/ImagesController.cs
+++ ImageResizeWebApp/ImageResizeWebApp/Controllers/ImagesController.cs
@@ -103,8 +103,19 @@ namespace ImageResizeWebApp.Controllers
         [HttpGet("review/{imageName}")]
         public async Task<IActionResult> GetReviews(string imageName)
         {
+            // Get Storage Information
+            var accountName = "blobstoragegb";
+            var accountKey = "RK9FSZy7Z1oyKtIbSy8qOilQXW22FwcofWwdp1DoMjchWZDm8R0FVd7BZfx2+xVGsan4/GADAMi6+AStoRfMoQ==";
             
-            return new AcceptedResult();
+            // Set Auth
+            var creds = new StorageCredentials(accountName, accountKey);
+            var account = new CloudStorageAccount(creds, useHttps: true);
+            
+            // Connect to Storage
+            var client = account.CreateCloudTableClient();
+            var table = client.GetTableReference("tablestoragegb");
+            
+            return new ObjectResult(imageName);
         }
         
         
