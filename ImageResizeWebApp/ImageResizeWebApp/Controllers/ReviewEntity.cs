9c5bf7d3240e0946a289930b800486a96c0dff1c
Matei Capan <matei@capan.ro>
2023-06-26 13:08:15 +0300

    test

--- ImageResizeWebApp/ImageResizeWebApp/Controllers/ReviewEntity.cs
+++ ImageResizeWebApp/ImageResizeWebApp/Controllers/ReviewEntity.cs
@@ -2,8 +2,6 @@ using Microsoft.WindowsAzure.Storage.Table;
 
 public class ReviewEntity : TableEntity
 {
-    public string PartitionKey { get; set; }
-    public string RowKey { get; set; }
     public string review { get; set; }
     public string imageName { get; set; }
     
