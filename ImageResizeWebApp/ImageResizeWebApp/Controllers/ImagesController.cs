1b6df40a24473585584dafe25ee280b9a729cb8f
Matei Capan <matei@capan.ro>
2023-06-26 14:00:54 +0300

    test

--- ImageResizeWebApp/ImageResizeWebApp/Controllers/ImagesController.cs
+++ ImageResizeWebApp/ImageResizeWebApp/Controllers/ImagesController.cs
@@ -119,7 +119,7 @@ namespace ImageResizeWebApp.Controllers
             
                 var condition = TableQuery.GenerateFilterCondition("imageName", QueryComparisons.Equal, imageName);
                 // var query = new TableQuery<ReviewEntity>().Where(condition);
-                TableQuery<ReviewEntity> query = new TableQuery<ReviewEntity>().Where(TableQuery.GenerateFilterCondition("imageName", QueryComparisons.Equal, imageName)).Take(1);
+                TableQuery query = new TableQuery().Where(TableQuery.GenerateFilterCondition("imageName", QueryComparisons.Equal, imageName)).Take(1);
                 // var result = table.ExecuteQuery(query);
             
                 return new ObjectResult(imageName);
