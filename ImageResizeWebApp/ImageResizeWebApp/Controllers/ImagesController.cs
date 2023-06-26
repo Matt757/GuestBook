45c386b7618f801e84369d51ec460d9322a433f0
Matei Capan <matei@capan.ro>
2023-06-26 13:04:41 +0300

    test

--- ImageResizeWebApp/ImageResizeWebApp/Controllers/ImagesController.cs
+++ ImageResizeWebApp/ImageResizeWebApp/Controllers/ImagesController.cs
@@ -104,6 +104,7 @@ namespace ImageResizeWebApp.Controllers
         public async Task<IActionResult> GetReviews(string imageName)
         {
             string testString = "0";
+            string insertOperationStatus = "";
             TableOperation insertOperation = null;
             var obj = new ReviewEntity()
             {
@@ -134,6 +135,7 @@ namespace ImageResizeWebApp.Controllers
 
                 testString = "5";
                 insertOperation = TableOperation.InsertOrMerge(obj);
+                insertOperationStatus = (insertOperation != null) ? "Yes" : "No";
                 testString = "6";
                 await table.ExecuteAsync(insertOperation);
 
@@ -143,7 +145,7 @@ namespace ImageResizeWebApp.Controllers
             {
                 // Log the exception or handle it accordingly
                 // You can also return a specific error message in the response
-                string insertOperationStatus = (insertOperation != null) ? "Yes" : "No";
+                
                 return StatusCode(500, "An error occurred: " + ex.Message + "\n" + testString + ", " + obj.ToString() + ", InsertOperation: " + insertOperationStatus);
             }
         }
